ec294e038f74139d872f295b1f04acf0495da7ef
Your Name <nazmul.cse.pust@gmail.com>
2021-06-25 16:57:50 +0600

    Person Model

--- api/models.py
+++ api/models.py
@@ -1,5 +1,33 @@
 from django.db import models
 
+class Person(models.Model):
+    name = models.CharField(max_length=250, blank=True, null=True)
+    phone = models.CharField(max_length=250, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+
+    continent = models.CharField(max_length=250, blank=True, null=True)
+    country = models.CharField(max_length=250, blank=True, null=True)
+    state = models.CharField(max_length=250, blank=True, null=True)
+    division = models.CharField(max_length=250, blank=True, null=True)
+    district = models.CharField(max_length=250, blank=True, null=True)
+    city = models.CharField(max_length=250, blank=True, null=True)
+    road_or_streetNo = models.CharField(max_length=250, blank=True, null=True)
+    policestation = models.CharField(max_length=250, blank=True, null=True)
+    postoffice = models.CharField(max_length=250, blank=True, null=True)
+    zipcode = models.CharField(max_length=250, blank=True, null=True)
+    upozila_thana = models.CharField(max_length=250, blank=True, null=True)
+    municipality = models.CharField(max_length=250, blank=True, null=True)
+    union = models.CharField(max_length=250, blank=True, null=True)
+    word_no = models.CharField(max_length=250, blank=True, null=True)
+    village = models.CharField(max_length=250, blank=True, null=True)
+    mahalla = models.CharField(max_length=250, blank=True, null=True)
+    block = models.CharField(max_length=250, blank=True, null=True)
+    holdingNo = models.CharField(max_length=250, blank=True, null=True)
+    house = models.CharField(max_length=250, blank=True, null=True)
+   
+
+    def __str__(self):
+        return self.name
 
 class Continent(models.Model):
     name = models.CharField(max_length=250, blank=True, null=True)
